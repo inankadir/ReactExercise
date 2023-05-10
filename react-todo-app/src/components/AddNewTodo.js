c9d1ae14099c1f0492803d397ebdc999a63a862a
kadir <idia_2008@hotmail.com>
2023-05-10 17:28:46 +0300

    add new file

--- react-todo-app/src/components/AddNewTodo.js
+++ react-todo-app/src/components/AddNewTodo.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 function AddNewTodo() {
-    return(
-        <div className="AddNewTodo">
-            AddNewTodo
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div className="AddNewTodo">
+      <div className="btn">
+        <button onClick={() => setShowModal(true)}>
+            + New Todo
+        </button>
+      </div>
+
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <div>
+            Hello World
+            <button onClick={() => setShowModal(false)}>
+                hide
+            </button>
         </div>
-    )
+      </Modal>
+    </div>
+  );
 }
 
-export default AddNewTodo
+export default AddNewTodo;
